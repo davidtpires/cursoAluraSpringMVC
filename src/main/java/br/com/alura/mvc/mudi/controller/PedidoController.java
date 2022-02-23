1fd5bbf7feb192db0de129ce1fb6ac02d590c2c5
David Trindade <davidt.info@gmail.com>
2022-02-23 20:08:02 -0300

    Modelando o status

--- src/main/java/br/com/alura/mvc/mudi/controller/PedidoController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/PedidoController.java
@@ -33,8 +33,7 @@ public class PedidoController {
 		
 		Pedido pedido = requisicao.toPedido();
 		PedidoRepository.save(pedido);
-		
-		return "pedido/formulario";
+		return "redirect:/home";
 	}
 
 }
